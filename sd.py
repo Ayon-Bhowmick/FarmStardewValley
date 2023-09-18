2fdddba7ede8f35621cc3cf923446ab96a5ffc6e
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-09-18 11:55:58 -0400

    first commit

---
+++ sd.py
@@ -0,0 +1,17 @@
+import os
+from sys import platform
+import shutil
+from datetime import date, datetime
+import pytz
+import subprocess
+
+SAVE_PATH_WIN = "C:\\Users\\{os.getlogin()}\\AppData\\Roaming\\StardewValley\\Saves"
+# TODO: add save file name
+
+def push():
+    shutil.copyfile(save_path, os.getcwd() + f"\\{FILENAME}")
+    os.system("git add *")
+    current_time = datetime.now(pytz.timezone("America/New_York"))
+    current_time_str = current_time.strftime("%H:%M:%S")
+    os.system(f"git commit -m \"Saved {date.today()} {current_time_str}\"")
+    os.system("git push")
